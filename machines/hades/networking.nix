c6430524ff9f15acc1a67728f68e45600e4f7fbd
adman44532 <adman44532@gmail.com>
2025-06-22 10:16:52 +1000

    fix(hades): parse files correctly

--- machines/hades/networking.nix
+++ machines/hades/networking.nix
@@ -1,6 +1,16 @@
-{...}: {
+{lib, ...}: let
+  tcp-ports-file = builtins.readFile ../../secrets/allowedTCPPorts.txt;
+  tcp-lines = lib.strings.splitString "\n" tcp-ports-file;
+  tcp-non-empty-lines = lib.lists.filter (s: s != "") tcp-lines;
+  tcp-ports = map lib.strings.toIntBase10 tcp-non-empty-lines;
+
+  udp-ports-file = builtins.readFile ../../secrets/allowedUDPPorts.txt;
+  udp-lines = lib.strings.splitString "\n" udp-ports-file;
+  udp-non-empty-lines = lib.lists.filter (s: s != "") udp-lines;
+  udp-ports = map lib.strings.toIntBase10 udp-non-empty-lines;
+in {
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = builtins.map builtins.stringToNumber (builtins.split "\n" (builtins.readFile "../../secrets/tcp-ports.txt"));
-  networking.firewall.allowedUDPPorts = builtins.map builtins.stringToNumber (builtins.split "\n" (builtins.readFile "../../secrets/udp-ports.txt"));
+  networking.firewall.allowedTCPPorts = tcp-ports;
+  networking.firewall.allowedUDPPorts = udp-ports;
   networking.firewall.trustedInterfaces = ["tailscale0"];
 }
--- machines/hades/ssh.nix
+++ machines/hades/ssh.nix
@@ -1,12 +1,12 @@
-{...}: {
+{lib, ...}: {
   services.openssh = {
     enable = true;
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "no";
       X11Forwarding = false;
-      AllowUsers = [builtins.readFile ../../secrets/username2.txt];
+      AllowUsers = [(lib.strings.removeSuffix "\n" (builtins.readFile ../../secrets/username2.txt))];
     };
-    ports = [(builtins.stringToNumber (builtins.readFile ../../secrets/ssh-port.txt))];
+    ports = [(lib.toInt (lib.strings.removeSuffix "\n" (builtins.readFile ../../secrets/ssh-port.txt)))];
   };
 }
