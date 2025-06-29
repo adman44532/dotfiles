bb51326b8bbbdd8b2948c7a14f9d63b457e381d3
adman44532 <adman44532@gmail.com>
2025-06-29 22:05:57 +1000

    wip: nvim and zellij
    Still tweaking these

--- machines/common/terminal/nvim.nix
+++ machines/common/terminal/nvim.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   # Note: nixd is LSP for Nix files, delcare in common's default file
-  environment.systemPackages = with pkgs; [lua-language-server gcc];
+  environment.systemPackages = with pkgs; [lua-language-server gcc prettierd python3 fd];
   programs.neovim = {
     enable = true;
     defaultEditor = true;
